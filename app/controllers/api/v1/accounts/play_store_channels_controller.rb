0dee03c8050d5bc9ce6d49ac997cb65b0967d1a5
“PrimeDevs” <primedevs.io@gmail.com>
2026-04-21 23:49:02 +0300

    fix(play-store): remove corrupted duplicate + correct controller class name

--- app/controllers/api/v1/accounts/play_store_channels_controller.rb
+++ app/controllers/api/v1/accounts/play_store_channels_controller.rb
@@ -1,4 +1,4 @@
-class Api::V1::Accounts::PlayStoreChannelController < Api::V1::Accounts::BaseController
+class Api::V1::Accounts::PlayStoreChannelsController < Api::V1::Accounts::BaseController
   def create
     creds_json = read_credentials
     channel = Channel::PlayStoreReviews.create!(
@@ -7,55 +7,18 @@ class Api::V1::Accounts::PlayStoreChannelController < Api::V1::Accounts::BaseCon
       service_account_email: parsed_email(creds_json),
       credentials_json: creds_json
     )
-
     inbox = Current.account.inboxes.create!(
       account: Current.account,
       channel: channel,
       name: permitted_params[:name].presence || permitted_params[:package_name]
     )
-
     render json: { id: inbox.id, name: inbox.name, channel_type: 'Channel::PlayStoreReviews' }
   rescue ActiveRecord::RecordInvalid => e
     render json: { error: e.message }, status: :unprocessable_entity
   end
-
   private
-
   def permitted_params
     params.permit(:package_name, :name, :credentials_json, :credentials_file)
-  end
-
-  def read_credentials
-    if permitted_params[:credentials_file].present?
-      permitted_params[:credentials_file].read
-    else
-      permitted_params[:credentials_json].to_s
-    end
-  end
-
-  def parsed_email(json)
-    JSON.parse(json)['client_email']
-  rescue StandardError
-    nil
-  end
-end
-
-class Api::V1::Accounts::PlayStoreChannelController < Api::V1::Accounts::class Api::V1::Accounts::Pla    creds_json = read_credentials
-                    l::PlayStoreReviews.create!(
-      account: Current.account,
-      package_name: permitted_params[:package_name],
-      service_account_email: parsed_email(creds_json),
-      credentials_json: creds_json
-    )
-    inbox =    inbox =    inbox =    inbox =    inbox =    inbox =    inbox =    inbox =    channel,
-      name: permitted_params[:name].presence || permitted_params[:package_name]
-    )
-    render json: { id: inbox.id, name: inbox.name, chan    render json: { id: yStoreReviews' }
-  rescue ActiveRecord::RecordInvalid => e
-    render json: { error: e.message }, status: :unprocessable_entity
-  end
-  end
-nder json: { error: e.messa    params.permit(:package_name, :name, :credentials_json, :credentials_file)
   end
   def read_credentials
     if permitted_params[:credentials_file].present?
